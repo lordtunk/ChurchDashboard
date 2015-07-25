9b3341d11a588521dc3a0080a0ba25c5e0e153f7
lordtunk <stevvensa.550@gmail.com>
2015-07-25 18:23:02 -0400

    Mostly finished upload feature

--- app/scripts/upload.js
+++ app/scripts/upload.js
@@ -2,224 +2,231 @@
     'use strict';
     if($('.upload-form').length === 0) return;
     
-    var ticket_id = '',
-        access_token = '',
-        ticket_uri = '',
-        upload_link = '',
-        complete_uri = '',
-        fileSize = 0,
-        fileType = 'video/mp4',
-        fileName = 'Temp',
-        progress = document.querySelector('.percent'),
-        cancel = false;
-
-    function parseFile (file) {
-        var chunkSize  = 64 * 1024; // bytes
-        var offset     = 0;
-        var self       = this; // we need a reference to the current object
-        var block      = null;
-        var start      = 0;
-        prepUpload();
-        function onRead (evt) {
-            if (evt.target.error == null) {
-                start = offset;
-                offset += evt.target.result.length;
-
-                if (offset >= fileSize) {
-                    $('#percent').text('100%');
-                    $('#status').text('Done!');
-                    return;
-                } else {
-                    var percent = ((offset/fileSize)*100).toFixed(2);
-                    $('#percent').text(percent+'%');
-                    $('#percent').css('width', percent+'%')
-                }
-                if(cancel === true) {
-                    $('#status').text('Cancelled');
-                    hideProgressBar();
-                } else {
-                    var cb = function() {
-                        block(offset, chunkSize, file);
-                    };
-                    // If the first read
-                    if(start === 0)
-                        uploadFirstChunk(evt.target.result, cb);
-                    else
-                        uploadChunk(start, offset, evt.target.result, cb);
-                }
-            } else {
-                console.log("Read error: " + evt.target.error);
-                return;
-            }
-        }
-
-        block = function (_offset, length, _file) {
-            var r = new FileReader();
-            var blob = _file.slice(_offset, length + _offset);
-            r.onload = onRead;
-            r.readAsText(blob);
-        };
-
-        block(offset, chunkSize, file);
-    }
-
-    function upload () {
-        var f = $('#files')[0];
-        if(f.files.length === 0) return;
-        fileSize = f.files[0].size;
-        fileType = f.files[0].type;
-        fileName = $('#file-name').text();
+    var access_token = '',
+        video_name = '',
+        email_address = '',
+        video_uri = '',
+        uploading = false;
+//        video_name = 'Test',
+//        email_address = 'stevvensa.550@gmail.com',
+//        video_uri = 'https://api.vimeo.com/videos/134261442';
+    
+    /**
+     * Called when files are dropped on to the drop target. For each file,
+     * uploads the content to Drive & displays the results when complete.
+     */
+    function handleFileSelect(evt) {
+        evt.stopPropagation();
+        evt.preventDefault();
+        if(uploading) return;
+        if(!validateFields()) return;
         
-        $('#upload-btn')[0].style.setProperty('display', 'none');
-        generateTicket(fileSize, function() {
-            parseFile(f.files[0]);
-        });
-    }
-
-    function cancelUpload () {
-        cancel = true;
-        $('#status').text('Cancelling...');
-    }
-
-    function prepUpload () {
-        cancel = false;
-        document.getElementById('progress-bar').className = 'loading';
-        document.getElementById('status').className = 'loading';
-        $('#cancel-btn')[0].style.setProperty('display', 'inline-block');
-        $('#percent').text('0%');
-        $('#status').text('Uploading...');
-        $('#percent').css('width', '0%');
+        var files = evt.dataTransfer.files; // FileList object.
+        
+        email = $('#email').val();
+        video_name = $('#file-name').val();
         $("#upload-settings *").attr("disabled", "disabled").off('click');
-    }
+        
+        
+        function callback() {
+            uploading = true;
+            var upgrade_to_1080 = document.getElementById("upgrade_to_1080").value;
+            document.getElementById('progress-bar').className = 'loading';
+            
+            // Clear the results div
+            var node = document.getElementById('results');
+            while (node.hasChildNodes()) node.removeChild(node.firstChild);
+            // Rest the progress bar
+            updateProgress(0);
 
+            var uploader = new MediaUploader({
+                file: files[0],
+                token: access_token,
+                upgrade_to_1080: upgrade_to_1080,
+                onError: function(data) {
+                    uploading = false;
+                    hideProgressBar();
+                    var errorResponse = JSON.parse(data);
+                    message = errorResponse.error;
+                    var element = document.createElement("div");
+                    element.setAttribute('class', "alert alert-danger");
+                    element.appendChild(document.createTextNode(message));
+                    document.getElementById('results').appendChild(element);
+                    
+                    email('3');
+                    $("#upload-settings *").attr("disabled", false).on('click');
+                },
+                onProgress: function(data) {
+                    updateProgress(data.loaded / data.total);
+                },
+                onComplete: function(videoId) {
+                    uploading = false;
+                    hideProgressBar();
+                    var url = 'https://vimeo.com/'+videoId;
+                    video_uri = 'https://api.vimeo.com/videos/'+videoId;
+                    
+                    var a = document.createElement('a');
+                    a.appendChild(document.createTextNode(url));
+                    a.setAttribute('href',url);
+                    var element = document.createElement("div");
+                    element.setAttribute('class', "alert alert-success");
+                    element.appendChild(a);
+
+                    document.getElementById('results').appendChild(element);
+                    
+                    updateTitle();
+                }
+            });
+            uploader.upload();
+        }
+        
+        checkQuota(files[0].size, callback);
+    }
+    
+    function validateFields() {
+        var msg = '';
+        if(!document.getElementById('email').validity.valid)
+            msg += 'Email address is invalid <br />';
+        if(!$('#file-name').val())
+            msg += 'File name cannot be empty';
+        if(msg) {
+            $().toastmessage('showErrorToast', msg);
+            return false;
+        }
+        return true;
+    }
+    
+    /**
+     * Dragover handler to set the drop effect.
+     */
+    function handleDragOver(evt) {
+        evt.stopPropagation();
+        evt.preventDefault();
+        evt.dataTransfer.dropEffect = (uploading) ? 'none' : 'copy'; 
+    }
+    /**
+     * Wire up drag & drop listeners once page loads
+     */
+    document.addEventListener('DOMContentLoaded', function () {
+        var dropZone = document.getElementById('drop_zone');
+        dropZone.addEventListener('dragover', handleDragOver, false);
+        dropZone.addEventListener('drop', handleFileSelect, false);
+    });
+    ;
+    /**
+     * Updat progress bar.
+     */
+    function updateProgress(progress) {
+        progress = Math.floor(progress * 100);
+        $('#percent').text(progress+'%');
+        $('#percent').css('width', progress+'%');
+    }
+    
     function hideProgressBar () {
-        $('#upload-btn')[0].style.setProperty('display', 'inline-block');
-        $('#cancel-btn')[0].style.setProperty('display', 'none');
-        $("#upload-settings *").attr("disabled", false).on('click');
+        $('#percent').text('0%');
+        $('#percent').css('width', '0%')
         setTimeout(function() { 
-            document.getElementById('progress-bar').className=''; 
-            document.getElementById('status').className='';
+            document.getElementById('progress-bar').className='';
         }, 2000);
     }
     
-    function uploadFirstChunk (data, length, cb) {
-        var me = this;
-        $.ajax({
-          type: 'PUT',
-          url: upload_link,
-          contentType: fileType || 'application/octet-stream',
-          processData: false,
-          data: data,
-          beforeSend: function( xhr ) {
-              //xhr.setRequestHeader( 'X-File-Name', fileName );
-              //xhr.setRequestHeader( 'Authorization', 'Bearer '+access_token );
-              //xhr.setRequestHeader( 'Content-Length', fileSize );
-              xhr.setRequestHeader( 'Content-Type', fileType );
-          }
-        })
-        .done(function(msg) {
-            console.log(msg);
-            verifyUpload(length, cb);
-        })
-        .fail(function(xhr, status, error) {
-            console.log(status, error);
-            $().toastmessage('showErrorToast', status+": Error verifying the upload: "+error);
-        });
+    function updateStatus(status) {
+        $('#status').text(status);
     }
     
-    function uploadChunk (startByte, endByte, data, cb) {
-        var me = this;
-        $.ajax({
-          type: 'PUT',
-          url: upload_link,
-          contentType: fileType || 'application/octet-stream',
-          processData: false,
-          data: data,
-          beforeSend: function( xhr ) {
-              //xhr.setRequestHeader( 'Authorization', 'Bearer '+access_token );
-              xhr.setRequestHeader( 'Content-Range', 'bytes '+startByte+'-'+endByte+'/'+fileSize );
-          }
-        })
-        .done(function(msg) {
-            console.log(msg);
-            verifyUpload(endByte-startByte, cb);
-        })
-        .fail(function(xhr, status, error) {
-            console.log(status, error);
-            $().toastmessage('showErrorToast', status+": Error verifying the upload: "+error);
-        });
-    }
-    
-    function generateTicket (size, cb) {
+    function checkQuota (size, cb) {
         var me = this;
         $('.upload-form').mask("Connecting to Vimeo...");
         $.ajax({
           type: 'POST',
-          url: 'ajax/generate_ticket.php',
+          url: 'ajax/check_quota.php',
           data: { size: size }
         })
         .done(function(msg) {
           $('.upload-form').unmask();
           var data = JSON.parse(msg);
           if(data.success) {
-              ticket_uri = data.ticket_uri;
-              ticket_id = data.ticket_id;
-              upload_link = data.upload_link;
-              complete_uri = data.complete_uri;
               access_token = data.access_token;
               cb.call(me);
           } else {
             if(data.msg) {
               $().toastmessage('showErrorToast', data.msg);
             } else {
-              $().toastmessage('showErrorToast', "Error establishing connection to Vimeo");
+              $().toastmessage('showErrorToast', "Error checking Vimeo upload quota");
             }
           }
         })
         .fail(function() {
               $('.upload-form').unmask();
-          $().toastmessage('showErrorToast', "Error searching people");
+          $().toastmessage('showErrorToast', "Error checking Vimeo upload quota");
         });
     }
     
-    function verifyUpload (offset, cb) {
+    function checkConversionStatus () {
         var me = this;
         $.ajax({
-          type: 'PUT',
-          url: upload_link,
-          beforeSend: function( xhr ) {
-              //xhr.setRequestHeader( 'Authorization', 'Bearer '+access_token );
-              //xhr.setRequestHeader( 'Content-Length', '0' );
-              xhr.setRequestHeader( 'Content-Range', 'bytes */*' );
-          }
-        })
-        .done(function(msg) {
-             console.log(msg);
-            // Verify that the Content-Range is from 0-offset
-            cb.call(me);
-        })
-        .fail(function(xhr, status, error) {
-            console.log(status, error);
-            $().toastmessage('showErrorToast', status+": Error verifying the upload: "+error);
-        });
-    }
-
-    function finishUpload () {
-        $.ajax({
-          url: upload_link,
-          type: 'DELETE',
+          type: 'GET',
+          url: video_uri,
           beforeSend: function( xhr ) {
             xhr.setRequestHeader( 'Authorization', 'Bearer '+access_token );
           }
-        }).success(function() {
-            $('#status').text('Successfully finished uploading the video');
-        }).failure(function() {
-            $('#status').text('Error finishing the upload video process');
+        })
+        .done(function(data) {
+          updateStatus('Upload complete! Video conversion status: '+data.status);
+          if(data.status !== "available") {
+              setTimeout(checkConversionStatus, 5000);
+          } else {
+              updateStatus('Upload complete! Video conversion complete!');
+              email('1');
+          }
+        })
+        .fail(function() {
+            $().toastmessage('showErrorToast', "Error checking the video conversion status");
+            email('3');
+            $("#upload-settings *").attr("disabled", false).on('click');
         });
     }
 
-    document.getElementById('upload-btn').addEventListener('click', upload);
-    document.getElementById('cancel-btn').addEventListener('click', cancelUpload);
+    function updateTitle () {
+        var me = this;
+        $.ajax({
+          type: 'PATCH',
+          url: video_uri,
+          beforeSend: function( xhr ) {
+            xhr.setRequestHeader( 'Authorization', 'Bearer '+access_token );
+          },
+          data: {
+              name: video_name
+          }
+        })
+        .done(function(data) {
+          checkConversionStatus();
+        })
+        .fail(function() {
+            $().toastmessage('showErrorToast', "Error checking the video conversion status");
+            email('4');
+            $("#upload-settings *").attr("disabled", false).on('click');
+        });
+    }
+    
+    function email (status) {
+        status = status || '2';
+        $.ajax({
+          type: 'POST',
+          url: 'ajax/send_email.php',
+          data: { email: email_address, video_status: status }
+        })
+        .done(function(data) {
+            $().toastmessage('showErrorToast', "Error sending status email");
+        })
+        .fail(function() {
+            $().toastmessage('showErrorToast', "Error sending status email");
+        });
+    }
+
+    //document.getElementById('upload-btn').addEventListener('click', upload);
+    //document.getElementById('cancel-btn').addEventListener('click', cancelUpload);
     
     checkLoginStatus();
+    //checkQuota(0, updateTitle);
 })();
